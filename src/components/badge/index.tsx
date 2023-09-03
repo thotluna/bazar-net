6193c6f600a54428b2a7c5eca203584bf19468d2
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-03 17:51:09 -0400

    feat: :lipstick: Improve ui

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,13 +1,14 @@
 interface Props {
   amount: number
+  color: string
 }
 
-export function Badge({ amount }: Props) {
+export function Badge({ amount, color = 'currentcolor' }: Props) {
   if (!amount || amount <= 0) return
   const num = amount >= 10 ? '+9' : amount
 
   return (
-    <div className="absolute top-[-12px] right-[-12px] w-6 h-6 bg-[var(--color-badge-bg)] flex items-center justify-center p-2 text-[var(--color-badge-text) ] rounded-full text-xs">
+    <div className="absolute top-[-3px] right-[-3px] w-5 h-5 bg-[var(--color-badge-bg)] flex items-center justify-center text-slate-950 rounded-full text-xs">
       {num}
     </div>
   )
