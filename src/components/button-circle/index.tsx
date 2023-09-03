fc73cde927015fbee70710e85633d6b464d10660
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-03 19:21:56 -0400

    style: :art: Format code

--- src/components/button-circle/index.tsx
+++ src/components/button-circle/index.tsx
@@ -1,19 +1,19 @@
 'use client'
 import { ReactNode } from 'react'
 
-interface Props{
+interface Props {
   children: ReactNode
   onClick: () => void
   active?: boolean
 }
-export function ButtonCircle({children, onClick,  active=false}: Props){
-
-  const style = active 
-    ? '-active'
-    : ''
+export function ButtonCircle({ children, onClick, active = false }: Props) {
+  const style = active ? '-active' : ''
 
   return (
-    <button onClick={onClick} className={`w-12 h-12  bg-[var(--color-button-circle-bg)] rounded-full border-2 border-[var(--color-button-circle-border${style})] text-[var(--color-button-circle-text${style})] flex items-center justify-center hover:scale-110 transition-transform duration-300` }>
+    <button
+      onClick={onClick}
+      className={`w-12 h-12  bg-[var(--color-button-circle-bg)] rounded-full border-2 border-[var(--color-button-circle-border${style})] text-[var(--color-button-circle-text${style})] flex items-center justify-center hover:scale-110 transition-transform duration-300`}
+    >
       {children}
     </button>
   )
--- src/components/like-button/index.tsx
+++ src/components/like-button/index.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { LikeIcon } from '..';
-import { ButtonCircle } from '../button-circle';
+import { LikeIcon } from '..'
+import { ButtonCircle } from '../button-circle'
 
-interface Props{
+interface Props {
   active?: boolean
 }
 
-export function LikeButton({active= false}: Props) {
+export function LikeButton({ active = false }: Props) {
   //TODO: add useLikedProduct, productBase prop and onClickHandler
 
   return (
-    <ButtonCircle onClick={()=>{}} active={active}>
+    <ButtonCircle onClick={() => {}} active={active}>
       <LikeIcon />
     </ButtonCircle>
   )
