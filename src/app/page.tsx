78b9f8182d04d8344227cb88dd578f1186ef0d7f
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-04 15:38:07 -0400

    feat: :sparkles: Create home page with search #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
-import { TopBar } from '@/components'
-import { BottonBar } from '@/components/botton-bar'
+import { CardProduct } from '@/components'
 import { SearchBar } from '@/components/search-bar'
+import { GetAllProducts } from '@/modules/products/application/get-all-products'
+import { ProductRepository } from '@/modules/products/domain'
+import { ApiProductRepository } from '@/modules/products/intraestructure/api-repository'
 
-export default function Home() {
+async function getAllProduct(query?: string) {
+  const repository: ProductRepository = ApiProductRepository
+  return GetAllProducts(repository, query)
+}
+
+export default async function Home() {
+  const product = await getAllProduct()
+
   return (
-    <main className="bg-[var(--color-bg)] h-full flex flex-col">
-      <TopBar />
-      <section className="flex-1 p-2">
-        <SearchBar />
+    <section className="flex-1 p-2 w-full">
+      <SearchBar />
+      <section
+        className="w-full mt-2 grid gap-4 justify-items-center place-content-start "
+        style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(8rem, 1fr))' }}
+      >
+        {product &&
+          product.products.length >= 0 &&
+          product.products.map((product) => <CardProduct key={product.id} product={product} />)}
       </section>
-      <BottonBar />
-    </main>
+    </section>
   )
 }
