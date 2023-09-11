dd62e6c6c6fbe5addad2462dac896c5c6d65cdc8
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-11 19:07:28 -0400

    feat: :building_construction: Create product detail page #3

--- src/components/bottom-product-detail/index.tsx
+++ src/components/bottom-product-detail/index.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { Product } from '@/modules/items/domain/product'
 import { useLikeStore } from '@/store/liked'
-import { ArrowBack, LikeButton } from '..'
+import { LikeButton } from '..'
+import { ButtonShopping } from '../button-shopping'
 
 export function BottomProductDetail({ product }: { product: Product }) {
   const exist = useLikeStore((state) => state.exist)
@@ -24,25 +25,14 @@ export function BottomProductDetail({ product }: { product: Product }) {
             (product.price * product.discountPercentage) / 100
           )}
         </span>
-        <span className="text-2xl font-semibold pt-2">
+        <span className="text-4xl font-semibold pt-2">
           {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
             product.price - (product.price * product.discountPercentage) / 100
           )}
         </span>
       </article>
       <LikeButton idProduct={product.id} active={exist(product.id)} />
-      <article className="flex flex-col items-center gap-1">
-        <h3 className="text-sm font-semibold">Product in shopping card:</h3>
-        <div className="w-full max-w-[141px] flex-1 flex items-center border-2 border-[var(--color-border)] rounded-full py-4 ">
-          <button>
-            <ArrowBack label="add" />
-          </button>
-          <span className="flex-1">0</span>
-          <button className="rotate-180">
-            <ArrowBack label="sum" />
-          </button>
-        </div>
-      </article>
+      <ButtonShopping id={product.id} />
     </section>
   )
 }
