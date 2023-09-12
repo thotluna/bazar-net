c5f03cbf7b665fcb59da288dec21dabedd050424
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-11 20:32:13 -0400

    refactor: :recycle: Make the import of the Badge component dynamic #3

--- src/components/shopping-car-button/index.tsx
+++ src/components/shopping-car-button/index.tsx
@@ -1,15 +1,16 @@
-import { useShoppingCarStore } from '@/store/shoppin-car'
+import { useShoppingCarStore } from '@/store/shopping-car'
+import dynamic from 'next/dynamic'
 import { useMemo } from 'react'
 import { ShoppingCardIcon } from '..'
-import { Badge } from '../badge'
 import { LinkButton } from '../link-button'
+const Badge = dynamic(() => import('../badge').then((mod) => mod.Badge), { ssr: false })
 
 interface Props {
   href: string
 }
 
 export function ShoppingCarButton({ href }: Props) {
-  const counts = useShoppingCarStore(state => state.ids)
+  const counts = useShoppingCarStore((state) => state.ids)
   const total = useMemo(() => {
     const totalArr = Object.values(counts)
     return totalArr.length === 0 ? 0 : totalArr.reduce((acc, value) => acc + value)
