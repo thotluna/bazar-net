916410d0d6d9e0189e11a5cb746d63fd7c0f1bfd
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-12 02:33:26 -0400

    fix: :bug: Inprove global shopping car state #3

--- src/components/button-shopping/index.tsx
+++ src/components/button-shopping/index.tsx
@@ -5,24 +5,32 @@ import { ArrowBack } from '..'
 
 export function ButtonShopping({ id, available }: { id: number; available: number }) {
   const count = useShoppingCarStore((state) => state.ids)
-  const add = useShoppingCarStore((state) => state.add)
-  const sub = useShoppingCarStore((state) => state.sub)
+  const save = useShoppingCarStore((state) => state.save)
 
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(count[id] ?? 0)
 
   useEffect(() => {
-    setCounter(count[id])
-  }, [count, id])
+    save(id, counter)
+  }, [counter, id, save])
+
+  const addHandler = () => {
+    available >= counter + 1 && setCounter((prev) => prev + 1)
+  }
+  const subHandler = () => {
+    counter - 1 >= 0 && setCounter((prev) => prev - 1)
+  }
 
   return (
     <article className="flex flex-col items-center gap-1">
       <h3 className="text-sm font-semibold">Product in shopping card:</h3>
       <div className="w-full max-w-[141px] flex-1 flex items-center border-2 border-[var(--color-border)] rounded-full py-2  ">
-        <button className="text-[var(--color-border)]" onClick={() => counter - 1 >= 0 && sub(id)}>
+        <button className="text-[var(--color-border)]" onClick={subHandler}>
           <ArrowBack label="subtract product" />
         </button>
-        <span className="flex-1 font-semibold text-1xl sm:text-2xl">{counter || 0} und</span>
-        <button className="rotate-180 text-[var(--color-border)]" onClick={() => available >= counter + 1 && add(id)}>
+        <span className="flex-1 font-semibold text-1xl sm:text-2xl" suppressHydrationWarning={true}>
+          {counter || 0} und
+        </span>
+        <button className="rotate-180 text-[var(--color-border)]" onClick={addHandler}>
           <ArrowBack label="add product" />
         </button>
       </div>
--- src/store/shopping-car.ts
+++ src/store/shopping-car.ts
@@ -8,8 +8,7 @@ import { create } from 'zustand'
 
 interface StateShoppingCar {
   ids: Record<number, number>
-  add: (id: number) => void
-  sub: (id: number) => void
+  save: (id: number, count: number) => void
   deleteAll: () => void
 }
 
@@ -17,26 +16,13 @@ const carRepository: ShoppingCarRepository = LocalStorageShoppingCarRepository()
 
 export const useShoppingCarStore = create<StateShoppingCar>((set, get) => ({
   ids: global.window !== undefined ? GetShoppingCar(carRepository) : {},
-  add: (id: number) => {
+  save: (id: number, count: number) => {
     set((state) => {
       const nState = {
         ...state,
         ids: {
           ...state.ids,
-          [id]: state.ids[id] ? state.ids[id] + 1 : 1
-        }
-      }
-      SaveShoppingCar(carRepository, nState.ids)
-      return nState
-    })
-  },
-  sub: (id: number) => {
-    set((state) => {
-      const nState = {
-        ...state,
-        ids: {
-          ...state.ids,
-          [id]: state.ids[id] && state.ids[id] > 1 ? state.ids[id] - 1 : 0
+          [id]: count
         }
       }
       SaveShoppingCar(carRepository, nState.ids)
