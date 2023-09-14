e4dcd6b8064e5fad30a660924cdc86fa5dd42eb7
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-14 17:19:32 -0400

    refactor: :recycle: Add name to botton return

--- src/components/empty-products/index.tsx
+++ src/components/empty-products/index.tsx
@@ -9,6 +9,7 @@ export function EmptyProducts({ message }: { message: string }) {
       <EmptyDraw width={300} height={240} />
       <h2 className="text-4xl">{message}</h2>
       <button
+        name="Return"
         onClick={() => route.back()}
         className="px-8 py-2 rounded-full bg-[var(--color-border)]  font-semibold hover:scale-105 transition-transform duration-200"
       >
