afbdbfb184ae0771ec12312a66c00de7840c2c18
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-19 22:59:28 -0400

    test: :wrench: Config unit test

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-const nextJest = require('next/jest')
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -6,10 +6,12 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
-const customJestConfig = {
+/** @type {import('jest').Config} */
+const config: import('jest').Config = {
+  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom'
+  testEnvironment: 'jsdom'
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+export default createJestConfig(config)
