3496e93eef01e48e1db3bbcdf3388779296eddab
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-09-20 19:07:22 -0400

    test: :bug: Error in intercept

--- cypress/e2e/app/products-liked/page.cy.tsx
+++ cypress/e2e/app/products-liked/page.cy.tsx
@@ -14,7 +14,7 @@ describe('Liked Products list', () => {
     likedProductsPageObject.elements.cards().should('have.length', 3)
   })
   it('should remove 1 card', () => {
-    cy.intercept('/api/items/group?id=3&id=4').as('saveNewList')
+    cy.intercept('/api/items/group*').as('saveNewList')
     homePageObject.clickLikeProduct(1)
     cy.wait('@saveNewList')
     likedProductsPageObject.elements.cards().should('have.length', 2)
